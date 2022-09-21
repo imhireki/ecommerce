c023206839a73aeaf729ab6385a915bad97b0b24
imHireki <imhireki@gmail.com>
2022-09-21 17:07:33 -0300

    [test]: add product app's e2e tests

---
+++ tests/test_product/e2e_tests.py
@@ -0,0 +1,56 @@
+import json
+
+from model_bakery import baker
+import pytest
+
+
+pytestmark = [pytest.mark.django_db, pytest.mark.e2e]
+
+
+class TestProductEndpoints:
+    endpoint = '/api/v1/products/'
+
+    def test_list_products(self, api_client):
+        baker.make('product.Product', 3)
+
+        response = api_client().get(self.endpoint)
+
+        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 3
+
+    def test_retrieve_product(self, api_client):
+        product = baker.make('product.Product')
+        variations = baker.make('product.ProductVariation', 2, product=product)
+        images = baker.make('product.ProductImage', 3, product=product)
+
+        expected_variations = [
+            {
+            "name": variation.name, "price": variation.price,
+            "promotional_price": variation.promotional_price
+            }
+            for variation in variations
+        ]
+
+        expected_json = {
+            "name": product.name,
+            "description": product.description,
+            "marketing_price": product.marketing_price,
+            "promotional_marketing_price": product.promotional_marketing_price,
+            "images": [image.image for image in images],
+            "variations": expected_variations
+        }
+         
+        response = api_client().get(f'{self.endpoint}{product.slug}/')
+
+        assert response.status_code == 200
+        assert json.loads(response.content) == expected_json
+
+    def test_search_products(self, api_client):
+        product_name = 'xyz'
+        baker.make('product.Product', name=product_name)
+
+        response = api_client().get(f'{self.endpoint}?search={product_name}')
+
+        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 1
+
