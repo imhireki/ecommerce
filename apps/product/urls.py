6b612d39cdec9ace198644a0f35a45a1b70b99d2
imHireki <imhireki@gmail.com>
2022-10-10 15:37:56 -0300

    [fix]: allow ProductDetailView's post method

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -6,9 +6,9 @@ from apps.product import views
 urlpatterns = [
     path('products/', views.ProductListView.as_view(),
          name='product-list'),
-    path('products/<slug:slug>/', views.ProductDetailView.as_view(),
-         name='product-detail'),
     path('products/search/', views.ProductSearchView.as_view(),
          name='product-search'),
+    path('products/<slug:slug>/', views.ProductDetailView.as_view(),
+         name='product-detail'),
 ]
 
