2620ed59c52af9bf936e410d1031bbc9a937de5c
imHireki <imhireki@gmail.com>
2022-10-18 12:10:53 -0300

    [test]: test using the new fixtures

--- tests/test_product/e2e_tests.py
+++ tests/test_product/e2e_tests.py
@@ -1,6 +1,5 @@
 import json
 
-from django.conf import settings
 from model_bakery import baker
 import pytest
 
@@ -11,44 +10,32 @@ pytestmark = [pytest.mark.django_db, pytest.mark.e2e]
 class TestProductEndpoints:
     endpoint = '/api/v1/products/'
 
-    def test_list_products(self, api_client, make_product_list_item, patch_image):
+    def test_list_products(self, api_client, patch_image,
+                           get_product_list_data):
         products = baker.make('product.Product', 2)
 
         response = api_client().get(self.endpoint)
 
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 2
-        assert make_product_list_item(products, 0) \
-               and make_product_list_item(products, 1) \
+        assert get_product_list_data(products[0]) \
+               and get_product_list_data(products[1]) \
                in json.loads(response.content)
 
-    def test_retrieve_product(self, api_client, patch_image):
+    def test_retrieve_product(self, api_client, patch_image,
+                              get_product_detail_data):
         product = baker.make('product.Product')
         variations = baker.make('product.ProductVariation', 2, product=product)
-        images = baker.make('product.ProductImage', 3, product=product)
-        
-        expected_json = {
-            "name": product.name,
-            "description": product.description,
-            "images": [{
-                'id': image.id,
-                'image_url': settings.ABSOLUTE_URL + '/media/image'
-            } for image in images],
-            "variations": [{
-                "id": variation.id,
-                "name": variation.name,
-                "quantity": variation.quantity,
-                "price": variation.price,
-                "promotional_price": variation.promotional_price,
-            } for variation in variations]
-        }
-         
+        images = baker.make('product.ProductImage', 5, product=product)
+
         response = api_client().get(f'{self.endpoint}{product.slug}/')
 
         assert response.status_code == 200
-        assert json.loads(response.content) == expected_json
+        assert json.loads(response.content) == get_product_detail_data(
+            product, images, variations)
 
-    def test_search_products(self, api_client, make_product_list_item, patch_image):
+    def test_search_products(self, api_client, patch_image,
+                             get_product_list_data):
         product = baker.make('product.Product', name='name')
 
         response = api_client().post(
@@ -58,6 +45,6 @@ class TestProductEndpoints:
 
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 1
-        assert make_product_list_item([product], 0) \
+        assert get_product_list_data(product) \
                in json.loads(response.content)
 
--- tests/test_product/test_serializers.py
+++ tests/test_product/test_serializers.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from model_bakery import baker
 import pytest
 
@@ -9,79 +8,37 @@ pytestmark = [pytest.mark.unit, pytest.mark.django_db]
 
 
 class TestProductListSerializer:
-    def test_serialize(self, mocker, patch_image):
+    def test_serialize(self, patch_image, get_product_list_data):
         product = baker.make('product.Product')
-
-        expected_data = {
-            'id': product.id,
-            'name': product.name,
-            'slug': settings.ABSOLUTE_URL + '/api/v1/products/' + product.slug,
-            'thumbnail_url': settings.ABSOLUTE_URL + '/media/thumbnail',
-            'marketing_price': str(product.marketing_price),
-            'promotional_marketing_price': str(
-                product.promotional_marketing_price)
-        }
-
         serializer = serializers.ProductListSerializer(product)
 
-        assert serializer.data == expected_data
+        assert serializer.data == get_product_list_data(product)
 
 
 class TestProductImageSerializer:
-    def test_serialize(self, mocker, patch_image):
+    def test_serialize(self, mocker, patch_image, get_product_image_data):
         image = baker.make('product.ProductImage')
-
-        mocker.patch.object(image.image, 'name', 'image')
-        expected_data = {
-            'id': image.id,
-            'image_url': settings.ABSOLUTE_URL + image.image.url
-        }
-
         serializer = serializers.ProductImageSerializer(image)
 
-        assert serializer.data == expected_data
+        assert serializer.data == get_product_image_data(image)
 
 
 class TestProductVariationSerializer:
-    def test_serialize(self, patch_image):
+    def test_serialize(self, patch_image, get_product_variation_data):
         variation = baker.make('product.ProductVariation')
-
-        expected_data = {
-            'id': variation.id,
-            'name': variation.name,
-            'quantity': variation.quantity,
-            'price': variation.price,
-            'promotional_price': variation.promotional_price
-        }
-
         serializer = serializers.ProductVariationSerializer(variation)
 
-        assert serializer.data == expected_data
+        assert serializer.data == get_product_variation_data(variation)
 
 
 class TestProductDetailSerializer:
-    def test_serialize(self, mocker, patch_image):
+    def test_serialize(self, patch_image, get_product_detail_data):
         product = baker.make('product.Product')
-        variation = baker.make('product.ProductVariation', product=product)
-        image = baker.make('product.ProductImage', product=product)
-
-        expected_data = {
-            'name': product.name,
-            'description': product.description,
-            'images': [{
-                'id': image.id,
-                'image_url': settings.ABSOLUTE_URL + '/media/image'
-            }],
-            'variations': [{
-                'id': variation.id,
-                'name': variation.name,
-                'quantity': variation.quantity,
-                'price': variation.price,
-                'promotional_price': variation.promotional_price
-            }]
-        }
+        variation = baker.make('product.ProductVariation', 2, product=product)
+        image = baker.make('product.ProductImage', 3, product=product)
 
         serializer = serializers.ProductDetailSerializer(product)
 
-        assert serializer.data == expected_data
+        assert serializer.data == get_product_detail_data(
+            product, image, variation)
 
