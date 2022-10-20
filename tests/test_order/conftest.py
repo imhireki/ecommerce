f0d57455ee586f4864413dab5b585ee41ffbecba
imHireki <imhireki@gmail.com>
2022-10-20 14:49:43 -0300

    [test]: update conftest fixtures

--- tests/test_order/conftest.py
+++ tests/test_order/conftest.py
@@ -1,35 +1,28 @@
+from rest_framework.serializers import DateTimeField
 import pytest
 
 
 @pytest.fixture
 def get_order_list_data():
     return lambda order, order_items: {
-        "id": order.id,
-        "paid_amount": order.paid_amount,
-        "status": order.status,
-        "items": [{
+        "id": str(order.id),
+        "paid_amount": str(order.paid_amount),
+        "status": order.get_status_display(),
+        "created_at": DateTimeField().to_representation(order.created_at),
+        "order_items": [{
             "id": order_item.id,
-            "product": {
-                "name": order_item.product.name,
-                "thumbnail_url": "",
-            },
-            "product_variation": {
-                "name": order_item.product_variation.name,
-                "price": order_item.product_variation.price,
-                "promotional_price": order_item.product_variation.\
-                                     promotional_price
-            },
-        } for order_item in order_items]
+            "product": order_item.product,
+            "product_variation": order_item.product_variation,
+            "price": str(order_item.price),
+            "quantity": order_item.quantity,
+        } for order_item in order_items],
     }
 
 @pytest.fixture
 def get_checkout_data():
-    return lambda product_variations: {
-        "id": "AbhDs-KLLzo-OZDh",
-        "price": sum([
-            variation.promotional_price
-            for variation in product_variations
-        ]),
-        "status": "Created",
+    return lambda order: {
+        "id": str(order.id),
+        "paid_amount": str(order.paid_amount),
+        "status": order.get_status_display(),
     }
 
