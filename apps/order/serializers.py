4d0d89fe61ad87e6115a082f4a7e44f1452e47cb
imHireki <imhireki@gmail.com>
2022-10-21 14:52:54 -0300

    [feat]: add OrderItemSerializer and OrderCheckoutSerializer

---
+++ apps/order/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.OrderItem
+        exclude = ['order']
+
+
+class OrderCheckoutSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, write_only=True)
+    status = serializers.CharField(source='get_status_display', required=False)
+
+    class Meta:
+        model = models.Order
+        exclude = ['created_at']
+        extra_kwargs = {
+            'status': {'write_only': True},
+            'user': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        order_items = validated_data.pop('order_items')
+
+        order_object = models.Order(**validated_data)
+        order_object.save()
+
+        for order_item in order_items:
+            order_item_object = models.OrderItem(
+                order=order_object, **order_item)
+            order_item_object.save()
+
+        return order_object
+
