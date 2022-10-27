2166777abfacc4d128a2c00bf0650a6a6ad17d06
imHireki <imhireki@gmail.com>
2022-10-27 18:03:08 -0300

    [test]: assert the response content; replace the module fixture for the conftest one

--- tests/test_product/test_views.py
+++ tests/test_product/test_views.py
@@ -9,14 +9,9 @@ from apps.product import views
 
 pytestmark = [pytest.mark.django_db, pytest.mark.unit]
 
-@pytest.fixture(autouse=True)
-def patch_resize_image(mocker):
-    mocker.patch('apps.product.utils.resize_image', lambda image, size: image)
-    
 
-def test_product_list_view(mocker, rf):
-    mocker.patch('apps.product.models.Product.get_thumbnail_absolute_url')
-    baker.make('product.Product', 3)
+def test_product_list_view(rf, get_product_list_data, patch_image):
+    products = baker.make('product.Product', 2)
 
     url = reverse('product-list')
     request = rf.get(url)
@@ -25,10 +20,15 @@ def test_product_list_view(mocker, rf):
     response = product_list_view(request).render()
 
     assert response.status_code == 200
-    assert len(json.loads(response.content)) == 3
+    assert len(json.loads(response.content)) == 2
+    assert get_product_list_data(products[0]) \
+        and get_product_list_data(products[1]) \
+        in json.loads(response.content)
 
-def test_product_detail_view(mocker, rf):
+def test_product_detail_view(rf, get_product_detail_data, patch_image):
     product = baker.make('product.Product')
+    images = baker.make('product.ProductImage', 2, product=product)
+    variations = baker.make('product.ProductVariation', 2, product=product)
 
     url = reverse('product-detail', kwargs={"slug": product.slug})
     request = rf.get(url)
@@ -37,19 +37,21 @@ def test_product_detail_view(mocker, rf):
     response = product_detail_view(request, slug=product.slug).render()
 
     assert response.status_code == 200
-    assert json.loads(response.content)
+    assert json.loads(response.content) \
+        == get_product_detail_data(product, images, variations)
 
-def test_product_search_view(mocker, rf):
-    mocker.patch('apps.product.models.Product.get_thumbnail_absolute_url')
-    product = baker.make('product.Product', 3)
+def test_product_search_view(rf, get_product_list_data, patch_image):
+    products = baker.make('product.Product', 3)
     
     url = reverse('product-search')
     request = rf.post(url, content_type='application/json',
-                      data={"query": product[0].name})
+                      data={"query": products[0].name})
     product_search_view = views.ProductSearchView.as_view()
 
     response = product_search_view(request).render()
 
     assert response.status_code == 200
-    assert json.loads(response.content)
+    assert len(json.loads(response.content)) == 1
+    assert get_product_list_data(products[0]) \
+           in json.loads(response.content)
 
