db8f240429d2ba1217dcd967e23169d574899ad2
imhireki <hireki@proton.me>
2025-01-04 14:02:21 -0300

    [chore]: add a proper product app config with lazy signals import

--- apps/product/apps.py
+++ apps/product/apps.py
@@ -1,2 +1,10 @@
-from . import signals
+from django.apps import AppConfig
+
+
+class ProductConfig(AppConfig):
+    name = 'apps.product'
+
+    def ready(self):
+        """Ensure signals aren't prematurely loaded."""
+        import apps.product.signals
 
