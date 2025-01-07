786482dc6ba3c590dae3d772742bb9ce6584c321
imhireki <hireki@proton.me>
2025-01-07 17:20:34 -0300

    [feat]: add report_yesterday_orders_to_staff_email

--- apps/order/utils.py
+++ apps/order/utils.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from apps.order.models import Order, OrderItem
 
@@ -24,4 +28,31 @@ def send_order_confirmation_email(instance: Order):
     message.extend(items)
 
     send_mail(subject, ''.join(message), None, [order.user.email])
+
+def report_yesterday_orders_to_staff_email():
+    yesterday = timezone.now() - timedelta(days=1)
+
+    orders = (
+        Order.objects.filter(created_at__date=yesterday)
+        .prefetch_related('order_items')
+    )
+    items = OrderItem.objects.filter(order__in=orders)
+
+    staff_members = [
+        staff.email for staff in 
+        get_user_model().objects.filter(is_staff=True)
+    ]
+
+    total = sum(order.paid_amount for order in orders)
+    num_diff_users = len({order.user for order in orders})
+    num_items = sum(item.quantity for item in items) 
+
+    subject = 'Django report: ' + yesterday.date().strftime("%d/%m/%Y")
+    message = [
+        f'total: {total}\n',
+        f'number of items: {num_items}\n',
+        f'number of different users: {num_diff_users}',
+    ]
+
+    send_mail(subject, ''.join(message), None, recipient_list=staff_members)
 
