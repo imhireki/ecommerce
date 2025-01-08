1ebac61efce08636ffe9781f007bd6e4ac9955fb
imhireki <hireki@proton.me>
2025-01-08 14:50:52 -0300

    [test]: replace plain emails with html ones

--- tests/test_order/test_utils.py
+++ tests/test_order/test_utils.py
@@ -1,36 +1,41 @@
 from datetime import timedelta
 
-from django.conf import settings
 from django.utils import timezone
+from django.conf import settings
 from django.core import mail
 from model_bakery import baker
 import pytest
 
+from apps.order import models
 from apps.order import utils
 
 
 pytestmark = [pytest.mark.django_db, pytest.mark.unit]
 
-def test_send_order_confirmation_email():
-    order = baker.make('order.Order', user__email='test@email.com')
-    items = baker.make('order.OrderItem', 2, order=order)
+def test_send_order_confirmation_email(mocker):
+    order = baker.make('order.Order', user__email='t1@ex.com')
+    baker.make('order.OrderItem', 2, order=order)
+
+    rts = mocker.patch('apps.order.utils.render_to_string',
+                       return_value='<html>email</html>')
 
     utils.send_order_confirmation_email(order)
-    subject = f'Order {order.id} at Django created'
-    message=[
-        f'Order {order.id} by {order.user.username}\n',
-        f'Total: {order.paid_amount}\n',
-        'Items:\n',
-        f'{items[0].quantity}x product {items[0].product_variation} ${items[0].price}\n',
-        f'{items[1].quantity}x product {items[1].product_variation} ${items[1].price}\n'
-    ]
 
+    context = {
+        'username': order.user.username,
+        'total': order.paid_amount,
+        'order': order.id,
+        'items': list(models.OrderItem.objects.filter(order=order.id))
+    }
+
+    assert rts.call_args[0][0] == 'emails/order_confirmation_email.html'
+    assert rts.call_args[0][1] == context 
     assert mail.outbox[0].to == [order.user.email]
     assert mail.outbox[0].from_email == settings.DEFAULT_FROM_EMAIL
-    assert mail.outbox[0].subject == subject
-    assert mail.outbox[0].body == ''.join(message)
+    assert mail.outbox[0].subject == '[E-commerce] Confirmed order'
+    assert mail.outbox[0].alternatives[0][0] == '<html>email</html>'
 
-def test_report_yesterday_orders_to_staff_email():
+def test_report_yesterday_orders_to_staff_email(mocker):
     yesterday = timezone.now() - timedelta(days=1)
 
     user = baker.make('User')
@@ -53,16 +58,23 @@ def test_report_yesterday_orders_to_staff_email():
     baker.make('order.Order', 3, created_at=timezone.now() - timedelta(days=2))
     baker.make('order.OrderItem', 2)
 
+    rts = mocker.patch('apps.order.utils.render_to_string',
+                       return_value='<html>email</html>')
+
     utils.report_yesterday_orders_to_staff_email()
 
-    subject = 'Django report: ' + yesterday.date().strftime("%d/%m/%Y")
-    message = [
-        'total: 20.00\n',
-        'number of items: 8\n',
-        'number of different users: 3',
-    ]
+    date = yesterday.date().strftime("%d/%m/%Y")
+    context = {
+        'date': date,
+        'total': 20.00,
+        'num_items': 8,
+        'num_diff_users': 3,
+    }
 
+    assert rts.call_args[0][0] == 'emails/daily_report.html'
+    assert rts.call_args[0][1] == context 
     assert mail.outbox[0].to == ['t1@ex.com', 't2@ex.com']
     assert mail.outbox[0].from_email == settings.DEFAULT_FROM_EMAIL
-    assert mail.outbox[0].subject == subject
-    assert mail.outbox[0].body == ''.join(message)
+    assert mail.outbox[0].subject == '[E-commerce] ' + date + ' report'
+    assert mail.outbox[0].alternatives[0][0] == '<html>email</html>'
+
