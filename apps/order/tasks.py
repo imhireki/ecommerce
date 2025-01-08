20d8750b1e598a57d6fd4876b487a065f25d87ef
imhireki <hireki@proton.me>
2025-01-08 16:58:40 -0300

    [feat]: add report_yesterday_orders_to_staff_email_async task

--- apps/order/tasks.py
+++ apps/order/tasks.py
@@ -9,3 +9,7 @@ def send_order_confirmation_email_async(order_id: str) -> None:
     order = Order.objects.get(id=order_id)
     utils.send_order_confirmation_email(order)
 
+@shared_task
+def report_yesterday_orders_to_staff_email_async() -> None:
+    utils.report_yesterday_orders_to_staff_email()
+
