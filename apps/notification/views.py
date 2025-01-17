59f0577d3a6fd47951835be9ad1931484802f7d5
imhireki <hireki@proton.me>
2025-01-17 18:12:23 -0300

    [feat]: add BroadcastListView

---
+++ apps/notification/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from django.utils import timezone
+
+from . import serializers
+from . import models
+
+
+class BroadcastListView(generics.ListAPIView):
+    serializer_class = serializers.BroadcastSerializer
+    
+    def get_queryset(self):
+        # Exclude broadcasts after current date
+        return models.Broadcast.objects.filter(
+            scheduled_to__lt=timezone.now()
+        )
+
