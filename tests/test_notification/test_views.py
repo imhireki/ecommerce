3b147a5656e1115737f7465e6cd20d2109cf23f4
imhireki <hireki@proton.me>
2025-01-17 18:12:23 -0300

    [test]: add test_broadcast_list_view

---
+++ tests/test_notification/test_views.py
@@ -0,0 +1,39 @@
+import json
+
+from django.utils import timezone
+from django.urls import reverse
+from datetime import timedelta
+from model_bakery import baker
+import pytest
+
+from apps.notification.views import BroadcastListView
+
+
+pytestmark = [pytest.mark.django_db, pytest.mark.unit]
+
+def test_broadcast_list_view(rf):
+    # Broadcasted
+    broadcasts = baker.make(
+        'notification.Broadcast', 3,
+        scheduled_to=timezone.now()-timedelta(days=1)
+    )
+
+    # Noise (to be broadcasted)
+    baker.make('notification.Broadcast', 2,
+               scheduled_to=timezone.now()+timedelta(days=1))
+
+    url = reverse('broadcast-notification-list')
+    request = rf.get(url)
+
+    view = BroadcastListView.as_view()
+
+    response = view(request).render()
+
+    expected_response = [{
+        'id': n.id, 'message': n.message,
+        'expires_at': n.expires_at.isoformat().replace('+00:00', 'Z')
+    } for n in broadcasts]
+
+    assert response.status_code == 200
+    assert json.loads(response.content) == expected_response
+
