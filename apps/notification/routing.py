d6edd78e1931f361a42d31add1a274a549e51a15
imhireki <hireki@proton.me>
2025-01-20 16:45:38 -0300

    [feat]: add WebSocket routing to the AsyncBroadcastConsumer

---
+++ apps/notification/routing.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import consumers
+
+
+websocket_urlpatterns = [
+    path('ws/notifications/broadcast/',
+         consumers.AsyncBroadcastConsumer.as_asgi()),
+]
