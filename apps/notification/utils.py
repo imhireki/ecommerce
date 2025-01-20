8115043298675004350caa756af0f78989989fdc
imhireki <hireki@proton.me>
2025-01-20 16:45:38 -0300

    [feat]: add delete_broadcast_object util

--- apps/notification/utils.py
+++ apps/notification/utils.py
@@ -1,8 +1,17 @@
+from django.db.models import ObjectDoesNotExist
 from asgiref.sync import async_to_sync 
 
 from .consumers import AsyncBroadcastConsumer
+from .models import Broadcast
 
 
 def broadcast(bid, message, expires_at) -> None:
     async_to_sync(AsyncBroadcastConsumer.broadcast)(bid, message, expires_at)
+
+def delete_broadcast_object(bid):
+    try:
+        broadcast = Broadcast.objects.get(id=bid)
+    except ObjectDoesNotExist:
+        return
+    broadcast.delete()
 
