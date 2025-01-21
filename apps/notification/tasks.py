bc73fe49f2bbe8853173a6b61e53f4ab03c84dc8
imhireki <hireki@proton.me>
2025-01-21 16:55:21 -0300

    [feat]: add broadcast_cleanup_task

--- apps/notification/tasks.py
+++ apps/notification/tasks.py
@@ -8,3 +8,14 @@ def broadcast_task(bid, message, expires_at) -> None:
     """Triggered at scheduled_to"""
     utils.broadcast(bid, message, expires_at)
 
+@shared_task
+def broadcast_cleanup_task(bid) -> None:
+    """Triggered at expires_at"""
+
+    # Remove periodic tasks
+    utils.remove_broadcast_periodic_task(f'broadcast {bid}')
+    utils.remove_broadcast_periodic_task(f'broadcast_cleanup {bid}')
+
+    # Delete my Broadcast object
+    utils.delete_broadcast_object(bid)
+
