b165042480b185e4e4010323e52369fb0ea5a85b
imhireki <hireki@proton.me>
2025-01-21 16:55:21 -0300

    [feat]: add remove_broadcast_periodic_task util

--- apps/notification/utils.py
+++ apps/notification/utils.py
@@ -1,3 +1,4 @@
+from django_celery_beat.models import PeriodicTask
 from django.db.models import ObjectDoesNotExist
 from asgiref.sync import async_to_sync 
 
@@ -14,4 +15,20 @@ def delete_broadcast_object(bid):
     except ObjectDoesNotExist:
         return
     broadcast.delete()
+
+def remove_broadcast_periodic_task(name: str) -> None:
+    try:
+        periodic_task = PeriodicTask.objects.get(name=name)
+    except ObjectDoesNotExist:
+        return
+
+    schedule = periodic_task.crontab
+
+    periodic_task.delete()
+
+    # Schedule being reused
+    if PeriodicTask.objects.filter(crontab=schedule).exists():
+        return
+
+    schedule.delete()
 
