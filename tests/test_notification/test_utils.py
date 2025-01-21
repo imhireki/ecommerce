e25ce7c700b414ae282451d4e2039b01fc3e00f4
imhireki <hireki@proton.me>
2025-01-21 16:55:21 -0300

    [test]: add test_remove_broadcast_periodic_task

--- tests/test_notification/test_utils.py
+++ tests/test_notification/test_utils.py
@@ -1,3 +1,4 @@
+from django_celery_beat.models import PeriodicTask, CrontabSchedule
 from model_bakery import baker
 import pytest
 
@@ -29,4 +30,26 @@ def test_delete_broadcast_object(broadcast):
     utils.delete_broadcast_object(9)
 
     assert not Broadcast.objects.filter(id=9).exists()
+
+@pytest.mark.parametrize('ptask, same_sched_ptask', [
+    (True,True), (True, False), (False, True), (False, False)
+])
+def test_remove_broadcast_periodic_task(mocker, ptask, same_sched_ptask):
+    schedule = None
+
+    if ptask or same_sched_ptask:
+        schedule = baker.make('CrontabSchedule')
+
+    if ptask:
+        baker.make('PeriodicTask', crontab=schedule, name='b 5')
+
+    if same_sched_ptask:
+        baker.make('PeriodicTask', crontab=schedule)
+
+    utils.remove_broadcast_periodic_task(name='b 5')
+
+    if ptask or same_sched_ptask:
+        assert (same_sched_ptask == CrontabSchedule.objects
+                .filter(id=schedule.id).exists())
+    assert not PeriodicTask.objects.filter(name='b 5').exists()
 
