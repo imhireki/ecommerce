90ce231f6b0d4c89975debc90f15507719880111
imhireki <hireki@proton.me>
2025-01-23 17:17:43 -0300

    [chore]: prepare fixtures to only call signals when needed

--- tests/test_notification/conftest.py
+++ tests/test_notification/conftest.py
@@ -1,9 +1,11 @@
 from channels.layers import InMemoryChannelLayer, get_channel_layer
+from django.db.models.signals import post_save, pre_delete
 from channels.testing import WebsocketCommunicator
 import pytest_asyncio
 import pytest
 
 from apps.notification import consumers
+from apps.notification import signals
 
 
 @pytest.fixture(autouse=True)
@@ -29,4 +31,28 @@ async def bc_communicator():
 @pytest.fixture
 def channel_layer() -> InMemoryChannelLayer:
     return get_channel_layer()  # type: ignore
+
+@pytest.fixture(autouse=True)
+def disable_broadcast_signals():
+    post_save.disconnect(signals.post_save_broadcast,
+                         sender='notification.Broadcast')
+    pre_delete.disconnect(signals.pre_delete_broadcast,
+                          sender='notification.Broadcast')
+
+    yield
+
+    post_save.connect(signals.post_save_broadcast,
+                      sender='notification.Broadcast')
+    pre_delete.connect(signals.pre_delete_broadcast,
+                       sender='notification.Broadcast')
+
+@pytest.fixture
+def enable_post_save_broadcast():
+    post_save.connect(signals.post_save_broadcast,
+                      sender='notification.Broadcast')
+
+@pytest.fixture
+def enable_pre_delete_broadcast():
+    pre_delete.connect(signals.pre_delete_broadcast,
+                       sender='notification.Broadcast')
 
