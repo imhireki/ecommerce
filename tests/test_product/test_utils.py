a3a3e999f35c547b30f7b6b6fcb41b17d0311d7d
imhireki <hireki@proton.me>
2025-01-31 13:37:57 -0300

    [test]: replace test_resize_image with test_make_thumbnail

--- tests/test_product/test_utils.py
+++ tests/test_product/test_utils.py
@@ -1,17 +1,23 @@
+from PIL.Image import Resampling
 import pytest
 
 from apps.product import utils
 
 
 @pytest.mark.unit
-def test_resize_image(mocker):
-    new_size = (512, 512)
-    django_file = mocker.patch("apps.product.utils.File")
-    pillow_image = mocker.patch("PIL.Image.open").return_value.__enter__.return_value
-    resized_image_mock = pillow_image.resize.return_value
+def test_make_thumbnail(mocker):
+    thumbnail_io = mocker.patch("apps.product.utils.BytesIO")()
+    content = mocker.patch("apps.product.utils.ContentFile")
+    pil_img = mocker.patch("PIL.Image.open").return_value.__enter__.return_value
+    resized_img = pil_img.resize.return_value
 
-    resized_image = utils.resize_image(mocker.Mock(), new_size)
+    size = (640, 360)
+    image = mocker.Mock()
+    thumb = utils.make_thumbnail(image, size)
 
-    assert resized_image == django_file.return_value
-    pillow_image.resize.assert_called_with(new_size, resample=1, reducing_gap=2)
-    resized_image_mock.save.assert_called()
+    pil_img.resize.assert_called_with(size, Resampling.LANCZOS, reducing_gap=2)
+    resized_img.save.assert_called_with(
+        thumbnail_io, pil_img.format, optimize=True, quality=85
+    )
+    content.assert_called_with(thumbnail_io.getvalue(), name=image.name)
+    assert thumb == content.return_value
